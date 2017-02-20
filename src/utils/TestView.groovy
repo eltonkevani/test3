f98cc59d0363440c86a3cf34ab974c3bdd80c5ba
eltonkevani <qevani.elton@gmail.com>
2017-02-20 18:18:30 +0100

    dummy commit

--- src/utils/TestView.groovy
+++ src/utils/TestView.groovy
@@ -4,10 +4,10 @@ import groovy.json.JsonSlurper
 
 class TestView implements Serializable {
     def steps
-    def jsonSlurper = new JsonSlurper()
+    //def jsonSlurper = new JsonSlurper()
     TestView(steps) {this.steps = steps}
     def mvn(text) {
-        def result = slurper.parseText(text)
+        def result = new JsonSlurper().parseText(text)
         steps.echo "${result['1']}"
     }
 }
