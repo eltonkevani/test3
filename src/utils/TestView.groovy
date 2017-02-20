1fedf89566ac4699ebcf4f7f5cbb2bdd8146570f
eltonkevani <qevani.elton@gmail.com>
2017-02-20 12:59:52 +0100

    dummy lib

--- src/utils/TestView.groovy
+++ src/utils/TestView.groovy
@@ -1,6 +1,13 @@
 package utils
 
-def checkOutFrom(repo) {
-    git url: "git@github.com:jenkinsci/${repo}"
-}
+//def checkOutFrom(repo) {
+ //   git url: "git@github.com:jenkinsci/${repo}"
+//}
 
+class TestView implements Serializable {
+    def steps
+    TestView(steps) {this.steps = steps}
+    def mvn(repo) {
+        steps.git url: "git@github.com:jenkinsci/${repo}"
+    }
+}
