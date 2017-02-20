9e3d9852cf5d7fc0e5b5e72ec44784a265d7ebae
eltonkevani <qevani.elton@gmail.com>
2017-02-20 13:24:53 +0100

    dummy lib

--- src/utils/TestView.groovy
+++ src/utils/TestView.groovy
@@ -1,8 +1,6 @@
 package utils
 
-//def checkOutFrom(repo) {
- //   git url: "git@github.com:jenkinsci/${repo}"
-//}
+/*
 
 class TestView implements Serializable {
     def steps
@@ -12,3 +10,106 @@ class TestView implements Serializable {
         steps.echo "lalalalalal"
     }
 }
+*/
+
+
+import groovyx.net.http.RESTClient
+import org.apache.http.HttpRequest
+import org.apache.http.HttpRequestInterceptor
+import org.apache.http.protocol.HttpContext
+
+import static groovyx.net.http.ContentType.JSON
+
+class TestView implements Serializable {
+
+
+    static def host = 'http://192.168.0.50:6516'
+    static def contextPath = ''
+
+
+    private RESTClient restClient
+    private projectName
+    private projectId
+    private enabled
+
+    private static def instances = [:]
+
+    public static TestView getInstance(String pn, enabled = true) {
+        if (pn in instances) {
+            return instances[pn]
+        } else {
+            println("TestView: creating new instance for project $pn")
+            instances.put(pn, new TestView(pn, enabled))
+            return instances[pn]
+        }
+    }
+
+    public TestView(pn, enabled = true) {
+        this.enabled = enabled
+        if (enabled) {
+            projectName = pn
+            restClient = createClient()
+            projectId = getOrCreateProject(projectName)
+        }
+    }
+
+    private RESTClient createClient() {
+
+        def username = 'admin'
+        def password = 'admin'
+
+
+        def client = new RESTClient(host)
+        client.client.addRequestInterceptor(new HttpRequestInterceptor() {
+            void process(HttpRequest httpRequest, HttpContext httpContext) {
+                httpRequest.addHeader('Authorization', 'Basic ' + (username + ":" + password).bytes.encodeBase64().toString())
+            }
+        })
+        return client
+    }
+
+
+    private getOrCreateProject(String projectName) {
+        def resp1;
+        def pid;
+        try {
+            resp1 = restClient.get(path: "$contextPath/api/internal/projects", contentType: JSON)
+            pid = resp1.data.find { it.title == projectName }
+        } catch (Exception e) {
+            println("TestView: Error trying to get project $projectName")
+        }
+
+        if (pid != null) {
+            return pid.name
+        }
+        // else continue...
+
+        def jsonbody = [
+                id: '',
+                type: 'xlt.Project',
+                title: projectName
+        ]
+
+        def resp;
+        try {
+            resp = restClient.post(path: "$contextPath/api/internal/projects",
+                    contentType: JSON,
+                    requestContentType: JSON,
+                    body: jsonbody)
+
+            return resp.data.name
+
+        } catch (Exception e) {
+           println("TestView: Error trying to create project $projectName")
+
+        }
+
+        return null
+    }
+
+
+
+
+
+}
+
