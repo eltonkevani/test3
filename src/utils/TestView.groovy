2a48566f33ee8cbf8a264582ddc0855a7c1594a9
eltonkevani <qevani.elton@gmail.com>
2017-02-21 09:41:53 +0100

    dymmy dymyy

--- src/utils/TestView.groovy
+++ src/utils/TestView.groovy
@@ -4,11 +4,14 @@ import groovy.json.JsonSlurper
 
 class TestView implements Serializable {
     def steps
-    //def jsonSlurper = new JsonSlurper()
+
     TestView(steps) {this.steps = steps}
     def mvn(text) {
         def result = new JsonSlurper().parseText(text)
-        steps.echo "${result['1']}"
+        if (result.title.contains("XL Deploy (master)")){
+            steps.echo "OK"
+        }
+        steps.echo "${result[1].title}"
     }
 }
 
