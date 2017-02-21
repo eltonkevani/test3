acacb20824aa7f089298c87de9560873c401550b
eltonkevani <qevani.elton@gmail.com>
2017-02-21 10:19:41 +0100

    dymmy dymyy

--- src/utils/TestView.groovy
+++ src/utils/TestView.groovy
@@ -4,15 +4,21 @@ import groovy.json.JsonSlurper
 
 class TestView implements Serializable {
     def steps
+    def XLTV_HOST = "http://192.168.0.50:6516"
+    def authentication = "xltv"
+    def contentType = "APPLICATION_JSON"
 
     TestView(steps) {this.steps = steps}
-    def mvn(text) {
-        def result = new JsonSlurper().parseText(text)
-        if (result.title.contains("XL Deploy (master)")){
-            steps.echo "OK"
+    def getOrCreateProject(text) {
+        def project = httpRequest url: "${XLTV_HOST}/api/v1/projects", authentication: "${authentication}", contentType: "${contentType}", acceptType: "${contentType}", httpMode: 'GET'
+        def result = new JsonSlurper().parseText(project.content)
+        if (result.title.contains(text)){
+            steps.echo "project exist"
         }
         steps.echo "${result[1].title}"
     }
+
+
 }
 
 
